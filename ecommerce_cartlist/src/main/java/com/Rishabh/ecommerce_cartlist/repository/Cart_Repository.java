f1448bf32d4453eac33f949dee5d94f67fa05374
rishabhkmishra2209 <89920949+rishabhkmishra2209@users.noreply.github.com>
2022-11-10 10:40:16 +0530

    Delete ecommerce_cartlist directory

--- ecommerce_cartlist/src/main/java/com/Rishabh/ecommerce_cartlist/repository/Cart_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.Rishabh.ecommerce_cartlist.repository;
-
-import com.Rishabh.ecommerce_cartlist.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Cart_Repository extends JpaRepository<Cart,Long> {
-}
